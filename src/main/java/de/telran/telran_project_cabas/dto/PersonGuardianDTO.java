ab53d09845734b64fee48d6a54734d3f72e9de81
Nataton <nataton02@gmail.com>
2022-07-08 13:42:00 +0200

    add create guardian

---
+++ src/main/java/de/telran/telran_project_cabas/dto/PersonGuardianDTO.java
@@ -0,0 +1,21 @@
+package de.telran.telran_project_cabas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class PersonGuardianDTO {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String phoneNumber;
+    private String email;
+}
